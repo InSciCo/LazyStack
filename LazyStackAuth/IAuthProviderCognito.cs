ca52b735b60d82e628ef14b4de668e9438d46b56
Tim May <tmay@inscico.com>
2021-07-19 09:08:13 -0700

    Added interface

---
+++ LazyStackAuth/IAuthProviderCognito.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amazon.CognitoIdentity;
+using Amazon.Extensions.CognitoAuthentication;
+
+namespace LazyStackAuth
+{
+    public interface IAuthProviderCognito : IAuthProvider
+    {
+        public CognitoUser CognitoUser { get; }
+        public CognitoAWSCredentials Credentials { get; }
+    }
+}
