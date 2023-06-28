b148f54fc24723de1c105a9e97d25ac4b04b683c
Timothy May <tmay@inscico.com>
2023-06-28 07:01:38 -0700

    Interim

--- LazyStackDynamoDBRepo/IDYDBRepository.cs
+++ LazyStackDynamoDBRepo/IDYDBRepository.cs
@@ -31,82 +31,117 @@ public interface IDYDBRepository<TEnv,T>
     Task FlushCache(string table = null);
 
     /// <summary>
-    /// Call CreateEAsync and return T
+    /// Create record
     /// </summary>
     /// <param name="data"></param>
-    /// <returns>ActionResult</returns>
+    /// <returns>ActionResult<typeparamref name="T"/></returns>
     Task<ActionResult<T>> CreateAsync(T data, string table = null, bool? useCache = null);
-
-    Task<ActionResult<T>> CreateNAsync(string topicId, string payloadParentId, string payloadId,  T data, string table = null, bool? useCache = null);
-
     /// <summary>
-    /// Create PutItemRequest and return pair (TEnv, T)
+    ///  Create record, Produce notification
+    /// </summary>
+    /// <param name="topicId"></param>
+    /// <param name="payloadParentId"></param>
+    /// <param name="payloadId"></param>
+    /// <param name="data"></param>
+    /// <param name="table"></param>
+    /// <param name="useCache"></param>
+    /// <returns>ActionResult<typeparamref name="T"/></returns>
+    Task<ActionResult<T>> CreateNAsync(string topicId, string payloadParentId, string payloadId,  T data, string table = null, bool? useCache = null);
+    /// <summary>
+    /// Create data Envelope
     /// </summary>
     /// <param name="data"></param>
     /// <param name="table"></param>
     /// <param name="useCache"></param>
-    /// <returns></returns>
+    /// <returns>ActionResult<typeparamref name="TEnv"/></returns>
     Task<ActionResult<TEnv>> CreateEAsync(T data, string table = null, bool? useCache = null);
-
     /// <summary>
-    /// Read data entity (calls ReadEAsync)
+    /// Read record
     /// </summary>
     /// <param name="pK"></param>
     /// <param name="sK"></param>
-    /// <returns>ActionResult<T></returns>
+    /// <returns>ActionResult<typeparamref name="T"/></returns>
     Task<ActionResult<T>> ReadAsync(string pK, string sK = null, string table = null, bool? useCache = null);
-
-
     /// <summary>
-    /// Read dBrecord (envelope)
+    /// Read record Envelope
     /// </summary>
     /// <param name="pK"></param>
     /// <param name="sK"></param>
-    /// <returns></returns>
+    /// <returns>AcitonResult<typeparamref name="TEnv"/></returns>
     Task<ActionResult<TEnv>> ReadEAsync(string pK, string sK = null, string table = null, bool? useCache = null);
-
     /// <summary>
-    /// Call CreateEAsync and return T
+    /// Update record
     /// </summary>
     /// <param name="data"></param>
-    /// <returns></returns>
+    /// <returns>ActionResult<typeparamref name="T"/></returns>
     Task<ActionResult<T>> UpdateAsync(T data, string table = null);
-
+    /// <summary>
+    /// Update record, produce Notification
+    /// </summary>
+    /// <param name="topicId"></param>
+    /// <param name="payloadParentId"></param>
+    /// <param name="payloadId"></param>
+    /// <param name="data"></param>
+    /// <param name="table"></param>
+    /// <returns><ActionResult<typeparamref name="T"/></ActionResult></returns>
     Task<ActionResult<T>> UpdateNAsync(string topicId, string? payloadParentId, string payloadId, T data, string table = null);
+    /// <summary>
+    /// Update record Envelope
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="table"></param>
+    /// <returns>ActionResult<typeparamref name="TEnv"/></returns>
     Task<ActionResult<TEnv>> UpdateEAsync(T data, string table = null);
 
 
     /// <summary>
-    /// Create PutItemRequest and call PutItmeAsync. Use UpdateUtcTick to do optimistic lock. Return T
+    /// Delete record. 
     /// </summary>
     /// <param name="pK"></param>
     /// <param name="sK"></param>
-    /// <returns></returns>
+    /// <returns>StatusCodeResult</returns>
     Task<StatusCodeResult> DeleteAsync(string pK, string sK = null, string table = null);
+    /// <summary>
+    /// Delete record, produce notification
+    /// </summary>
+    /// <param name="topicId"></param>
+    /// <param name="payloadParentId"></param>
+    /// <param name="payloadId"></param>
+    /// <param name="pK"></param>
+    /// <param name="sK"></param>
+    /// <param name="table"></param>
+    /// <returns>StatusCodeResult</returns>
     Task<StatusCodeResult> DeleteNAsync(string topicId, string payloadParentId, string payloadId, string pK, string sK = null, string table = null);
+    /// <summary>
+    /// Delete record Envelope
+    /// </summary>
+    /// <param name="pK"></param>
+    /// <param name="sK"></param>
+    /// <param name="table"></param>
+    /// <returns>ActionResult<typeparamref name="TEnv"/></returns>
     Task<ActionResult<TEnv>> DeleteEAsync(string pK, string sK = null, string table = null);
 
     /// <summary>
     /// Call QueryAsync and return list of envelopes
     /// </summary>
     /// <param name="queryRequest"></param>
-    /// <returns>List<TEnv></TEnv></returns>
+    /// <returns>List<typeparamref name="TEnv"/></returns>
     Task<ActionResult<ICollection<TEnv>>> ListEAsync(QueryRequest queryRequest, bool? useCache = null);
 
     /// <summary>
     /// Call QueryAsync and return list of data objects of type T
     /// </summary>
     /// <param name="queryRequest"></param>
-    /// <returns>List<T></returns>
+    /// <returns>List<typeparamref name="T"/></returns>
     Task<ActionResult<ICollection<T>>> ListAsync(QueryRequest queryRequest, bool? useCache = null);
 
     /// <summary>
-    /// Return all records having the primary key value specified
+    /// Return a simple query request for records matching query arguments
     /// </summary>
     /// <param name="pK"></param>
     /// <param name="expressionAttributeNames"></param>
     /// <param name="projectionExpression"></param>
-    /// <returns></returns>
+    /// <returns>QueryRequest</returns>
     QueryRequest QueryEquals(string pK, Dictionary<string, string> expressionAttributeNames = null, string projectionExpression = null, string table = null);
 
     /// <summary>
@@ -116,7 +151,7 @@ public interface IDYDBRepository<TEnv,T>
     /// <param name="keyField"></param>
     /// <param name="key"></param>
     /// <param name="expressionAttributeNames"></param>
-    /// <returns></returns>
+    /// <returns>QueryRequest</returns>
     QueryRequest QueryEquals(string pK, string keyField, string key, Dictionary<string, string> expressionAttributeNames = null, string projectionExpression = null, string table = null);
 
 
@@ -127,8 +162,19 @@ public interface IDYDBRepository<TEnv,T>
     /// <param name="keyField"></param>
     /// <param name="key"></param>
     /// <param name="expressionAttributeNames"></param>
-    /// <returns></returns>
+    /// <returns>QueryRequest</returns>
     QueryRequest QueryBeginsWith(string pK, string keyField, string key, Dictionary<string, string> expressionAttributeNames = null, string projectionExpression = null, string table = null);
-
+    /// <summary>
+    /// Create a notification
+    /// </summary>
+    /// <param name="topicId"></param>
+    /// <param name="payloadParent"></param>
+    /// <param name="payloadId"></param>
+    /// <param name="payload"></param>
+    /// <param name="payloadType"></param>
+    /// <param name="payloadAction"></param>
+    /// <param name="createdAt"></param>
+    /// <param name="table"></param>
+    /// <returns>ActionResult</returns>
     Task<ActionResult> CreateNotification(string topicId, string? payloadParent, string payloadId, string payload, string payloadType, string payloadAction, long createdAt, string table);
 }
